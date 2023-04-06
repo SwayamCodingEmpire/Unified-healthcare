53cf6368abf8d0672194517557579012374bba77
CoderSPM <111043295+CoderSPM@users.noreply.github.com>
2023-04-06 15:23:52 +0530

    Create Mainsrc.java

---
+++ Mainsrc.java
@@ -0,0 +1,72 @@
+class emergency
+{
+    String blood_group;
+    long phone_no;
+    String address;
+    String location;
+    long call_doctor(String name)
+    {
+        
+    }
+    final void emergenclass emergency
+{
+    String blood_group;
+    long phone_no;
+    String address;
+    String location;
+    long call_doctor(String name)
+    {
+        
+    }
+    final void emergency_call(long nearby_phone[])
+    {
+        
+    }
+}
+
+class health_records extends emergency
+{
+    String disease[];
+    String surgery[];
+    String inherited[];
+    String abnormality[];
+    
+}
+
+class patient extends health_records
+{
+    String name;
+    long aadhaar;
+    long health_card_number;
+    String find_doctor(String department)
+    {
+        
+    }
+        
+}
+cy_call(long nearby_phone[])
+    {
+        
+    }
+}
+
+class health_records extends emergency
+{
+    String disease[];
+    String surgery[];
+    String inherited[];
+    String abnormality[];
+    
+}
+
+class patient extends health_records
+{
+    String name;
+    long aadhaar;
+    long health_card_number;
+    String find_doctor(String department)
+    {
+        
+    }
+        
+}
